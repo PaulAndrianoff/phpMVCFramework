b1588c498284cd6f1420ceccce1d51efd780934c
PaulAndrianoff <pandrianoff@gmail.com>
2024-02-04 13:10:27 +0100

    Add styles

--- app/views/layout.php
+++ app/views/layout.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>App Name - <?php echo $title ?? 'Default Title'; ?></title>
+    <link rel="stylesheet" href="<?= $config['app']['base_url']; ?>css/main.css">
 </head>
 <body>
     <?php include('partials/header.php'); ?>
