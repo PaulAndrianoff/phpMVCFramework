8bf2a64b2898cbfd1ba47a6080697b4d48538179
PaulAndrianoff <pandrianoff@gmail.com>
2024-02-05 13:04:10 +0100

    Modify layout

--- app/views/layout.php
+++ app/views/layout.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>App Name - <?php echo $title ?? 'Default Title'; ?></title>
+    <title><?= $config['app']['app_name']; ?> - <?php echo $title ?? 'Default Title'; ?></title>
     <link rel="stylesheet" href="<?= $config['app']['base_url']; ?>css/main.css">
 </head>
 <body>
--- config/app.php
+++ config/app.php
@@ -1,6 +1,7 @@
 <?php
 return [
     'debug' => true,
+    'app_name' => 'PHP framework',
     'base_url' => 'http://localhost/Projects/phpFramework/public/',
     // Additional application settings...
 ];
