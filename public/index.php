bec7a9ceb23ac59ccb94e696ff73c08dcaddffc1
PaulAndrianoff <pandrianoff@gmail.com>
2024-02-05 14:56:10 +0100

    Init session

--- public/index.php
+++ public/index.php
@@ -1,6 +1,8 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use app\helpers\SessionHelper;
+
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
 $dotenv->load();
 
@@ -11,6 +13,8 @@ $scriptName = $_SERVER['SCRIPT_NAME'];
 // Remove the subdirectory part from the request URI
 $path = preg_replace('/^' . preg_quote(str_replace('/public', '', dirname($scriptName)), '/') . '/', '', $requestUri);
 $path = str_replace('/public', '', $path);
+
+SessionHelper::start();
 
 // Load the routes
 $router = require_once __DIR__ . '/../app/routes.php';
