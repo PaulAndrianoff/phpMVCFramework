ffc5f1c184d94e4cca6760b6ef20d4c3effb961a
PaulAndrianoff <pandrianoff@gmail.com>
2024-02-05 14:55:26 +0100

    First style

--- app/views/partials/header.php
+++ app/views/partials/header.php
@@ -1,3 +1,8 @@
 <header>
-    <h2>Header</h2>
+    <h2><a href="<?= $config['app']['base_url']; ?>"><?= $config['app']['app_name']; ?></a></h2>
+    <nav>
+        <ul>
+            <li><a href="<?= $config['app']['base_url']; ?>login">Login</a></li>
+        </ul>
+    </nav>
 </header>
