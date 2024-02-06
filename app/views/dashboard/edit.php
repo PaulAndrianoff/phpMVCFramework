2214b36d763c9f7e89d6d6241105721a7f4210e2
PaulAndrianoff <pandrianoff@gmail.com>
2024-02-06 17:28:39 +0100

    Modify style of form

--- app/views/dashboard/edit.php
+++ app/views/dashboard/edit.php
@@ -1,37 +1,35 @@
 <?php ob_start(); ?>
 
 <h1>Editing Table: <?= htmlspecialchars($table); ?></h1>
+<a class="button" href="<?= $config['app']['base_url']; ?>dashboard/new/<?= htmlspecialchars($table); ?>">+ add</a>
 
-<form action="#" method="post">
-    <a href="<?= $config['app']['base_url']; ?>dashboard/new/<?= htmlspecialchars($table); ?>">+ add</a>
-    <table>
-        <thead>
-            <tr>
-                <?php
-                if (!empty($data)) {
-                    foreach (array_keys($data[0]) as $column) {
-                        echo "<th>" . htmlspecialchars($column) . "</th>";
-                    }
-                    echo "<th>Actions</th>";
+<table>
+    <thead>
+        <tr>
+            <?php
+            if (!empty($data)) {
+                foreach (array_keys($data[0]) as $column) {
+                    echo "<th>" . htmlspecialchars($column) . "</th>";
                 }
-                ?>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($data as $row): ?>
-            <tr>
-                <?php foreach ($row as $value): ?>
-                    <td><?= htmlspecialchars($value); ?></td>
-                <?php endforeach; ?>
-                <td>
-                    <a href="<?= $config['app']['base_url']; ?>dashboard/edit/<?= htmlspecialchars($table); ?>/<?= $row['id']; ?>">Edit</a> |
-                    <a href="<?= $config['app']['base_url']; ?>dashboard/delete/<?= htmlspecialchars($table); ?>/<?= $row['id']; ?>" onclick="return confirm('Are you sure?');">Delete</a>
-                </td>
-            </tr>
+                echo "<th>Actions</th>";
+            }
+            ?>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($data as $row): ?>
+        <tr>
+            <?php foreach ($row as $value): ?>
+                <td><?= htmlspecialchars($value); ?></td>
             <?php endforeach; ?>
-        </tbody>
-    </table>
-</form>
+            <td>
+                <a href="<?= $config['app']['base_url']; ?>dashboard/edit/<?= htmlspecialchars($table); ?>/<?= $row['id']; ?>">Edit</a> |
+                <a href="<?= $config['app']['base_url']; ?>dashboard/delete/<?= htmlspecialchars($table); ?>/<?= $row['id']; ?>" onclick="return confirm('Are you sure?');">Delete</a>
+            </td>
+        </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
 
 <?php $content = ob_get_clean(); ?>
 
