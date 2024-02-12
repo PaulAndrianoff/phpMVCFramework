d8e89daf394a5032c44c2be857f87523eedb6ae1
PaulAndrianoff <pandrianoff@gmail.com>
2024-02-12 21:03:46 +0100

    Add wisiwig for textarea

--- app/views/partials/footer.php
+++ app/views/partials/footer.php
@@ -1,6 +1,6 @@
-<scripts>
+<script src="<?= getLink('js/tinymce.min.js'); ?>"></script>
+<script src="<?= getLink('js/tinymceConfig.js'); ?>"></script>
 <?php echo $scripts ?? ''; ?>
-</scripts>
 
 <footer class="footer">
     <h5>Footer</h5>
